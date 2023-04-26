30e22426ec1d83631264ca9faada257252d4a481
skftnt <129919482+skftnt@users.noreply.github.com>
2023-04-25 19:06:13 -0700

    Added Communication Control functions
    0.3.0

--- FortiEDRWrapper.py
+++ FortiEDRWrapper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env Python
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 import base64
 import requests
@@ -216,3 +216,221 @@ class FortiEDRWrapper:
             raise e
     else:
         return response.json()
+    
+        def list_products(self, params):
+        if self.organization:
+            params["organization"] = self.organization
+
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/list-products")
+
+        try:
+            response = self.session.get(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def set_policy_mode(self, policy_names, mode, organization=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/set-policy-mode")
+        params = {
+            "policyNames": ",".join(policy_names),
+            "mode": mode,
+        }
+        if organization:
+            params["organization"] = organization
+
+        try:
+            response = self.session.put(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def assign_collector_group(self, collector_groups, policy_name, organization=None, force_assign=False):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/assign-collector-group")
+        params = {
+            "collectorGroups": ",".join(collector_groups),
+            "policyName": policy_name,
+            "forceAssign": force_assign,
+        }
+        if organization:
+            params["organization"] = organization
+
+        try:
+            response = self.session.put(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def set_policy_permission(self, vendors, products, versions, policies, decision, signed=None, apply_nested=None,
+                              organization=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/set-policy-permission")
+        params = {
+            "vendors": ",".join(vendors),
+            "products": ",".join(products),
+            "versions": ",".join(versions),
+            "policies": ",".join(policies),
+            "decision": decision
+        }
+        if signed is not None:
+            params["signed"] = signed
+        if apply_nested is not None:
+            params["applyNested"] = apply_nested
+        if organization:
+            params["organization"] = organization
+
+        try:
+            response = self.session.put(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def clone_policy(self, source_policy_name, new_policy_name, organization=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/clone-policy")
+        params = {
+            "sourcePolicyName": source_policy_name,
+            "newPolicyName": new_policy_name
+        }
+        if organization:
+            params["organization"] = organization
+
+        try:
+            response = self.session.post(url, headers={"Content-Type": "application/json"}, params=params,
+                                         verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def list_policies(self, organization=None, policies=None, rules=None, sources=None, state=None, decisions=None,
+                      pageNumber=None, strictMode=False, itemsPerPage=100, sorting=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/list-policies")
+        params = {
+            "strictMode": strictMode,
+            "itemsPerPage": itemsPerPage
+        }
+        if organization:
+            params["organization"] = organization
+        if policies:
+            params["policies"] = ','.join(policies)
+        if rules:
+            params["rules"] = ','.join(rules)
+        if sources:
+            params["sources"] = ','.join(sources)
+        if state:
+            params["state"] = state
+        if decisions:
+            params["decisions"] = ','.join(decisions)
+        if pageNumber:
+            params["pageNumber"] = pageNumber
+        if sorting:
+            params["sorting"] = sorting
+
+        try:
+            response = self.session.get(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def resolve_applications(self, organization=None, vendors=None, products=None, versions=None,
+                             signed=None, applyNested=None, comment=None, resolve=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/resolve-applications")
+        params = {}
+        if organization:
+            params["organization"] = organization
+        if vendors:
+            params["vendors"] = ','.join(vendors)
+        if products:
+            params["products"] = ','.join(products)
+        if versions:
+            params["versions"] = ','.join(versions)
+        if signed is not None:
+            params["signed"] = signed
+        if applyNested is not None:
+            params["applyNested"] = applyNested
+        if comment:
+            params["comment"] = comment
+        if resolve is not None:
+            params["resolve"] = resolve
+
+        try:
+            response = self.session.put(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
+
+    def set_policy_rule_state(self, policyName, ruleName, state, organization=None):
+        url = urllib.parse.urljoin(self.base_url, "/management-rest/comm-control/set-policy-rule-state")
+        params = {
+            "policyName": policyName,
+            "ruleName": ruleName,
+            "state": state
+        }
+        if organization:
+            params["organization"] = organization
+
+        try:
+            response = self.session.put(url, headers={"Content-Type": "application/json"}, params=params,
+                                        verify=self.verify_ssl)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            if response.status_code == 400:
+                raise ValueError("Bad Request: The given parameters do not match the expected format or values range.")
+            elif response.status_code == 500:
+                raise ValueError("Internal Server Error: An unexpected error occurred. Contact Fortinet support.")
+            else:
+                raise e
+        else:
+            return response.json()
